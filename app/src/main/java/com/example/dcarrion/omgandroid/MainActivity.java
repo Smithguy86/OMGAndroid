088badff887cb8d6390c98c9639b514d17573d45
Danilo Carrion <adida948@hotmail.com>
2015-06-03 15:42:55 -0400

    Add logging when user presses ListView Item

--- app/src/main/java/com/example/dcarrion/omgandroid/MainActivity.java
+++ app/src/main/java/com/example/dcarrion/omgandroid/MainActivity.java
@@ -39,19 +39,19 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
 
         //Access the EditText defined in Layout xml
         mainEditText = (EditText) findViewById(R.id.main_edittext);
-
+//
         //Access the listView
         mainListView = (ListView) findViewById(R.id.main_listview);
-
+//
         // Create an ArrayAdapter for the ListView
         mArrayAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1,
                 mNameList);
-
+//
         //Set the listview to use the arrayAdapter
         mainListView.setAdapter(mArrayAdapter);
-
-        // Set this activity to react to the items being pressed
+//
+//        // Set this activity to react to the items being pressed
         mainListView.setOnItemClickListener(this);
     }
 
@@ -69,13 +69,13 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
      */
     @Override
     public void onClick(View v) {
-        //Test the button
-        mainTextView.setText("Button Pressed");
+//        //Test the button
+        //mainTextView.setText("Button Pressed");
 
-        //Take what was typed in the Edit Text and use it in the TextView
+//        //Take what was typed in the Edit Text and use it in the TextView
         mainTextView.setText(mainEditText.getText().toString() + " is learning Android Dev");
-
-        //Also add that value to the list shown in the listView
+//
+//        //Also add that value to the list shown in the listView
         mNameList.add(mainEditText.getText().toString());
         mArrayAdapter.notifyDataSetChanged();
 
@@ -98,7 +98,7 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-        //Log the item position and contents to the console in Debug
+//        //Log the item position and contents to the console in Debug
         Log.d("omg Android", position + ":" + mNameList.get(position));
 
     }
