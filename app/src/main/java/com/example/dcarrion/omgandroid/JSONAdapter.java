d8ae41ac4cc10f535eccdea11a84ff9da7d236f5
Danilo Carrion <adida948@hotmail.com>
2015-06-05 14:54:04 -0400

    Allow app to share data in textview.

---
+++ app/src/main/java/com/example/dcarrion/omgandroid/JSONAdapter.java
@@ -0,0 +1,168 @@
+package com.example.dcarrion.omgandroid;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by dcarrion on 6/4/2015.
+ */
+public class JSONAdapter extends BaseAdapter {
+    private static final String IMAGE_URL_BASE= "http://covers.openlibrary.org/b/id/";
+
+    Context mContext;
+    LayoutInflater mInflater;
+    JSONArray mJsonArray;
+
+    public JSONAdapter(Context context,LayoutInflater inflater) {
+        mContext=context;
+        mInflater =inflater;
+        mJsonArray = new JSONArray();
+    }
+
+    /**
+     * How many items are in the data set represented by this Adapter.
+     *
+     * @return Count of items.
+     */
+    @Override
+    public int getCount() {
+        return mJsonArray.length();
+    }
+
+    /**
+     * Get the data item associated with the specified position in the data set.
+     *
+     * @param position Position of the item whose data we want within the adapter's
+     *                 data set.
+     * @return The data at the specified position.
+     */
+    @Override
+    public Object getItem(int position) {
+        return mJsonArray.optJSONObject(position);
+    }
+
+    /**
+     * Get the row id associated with the specified position in the list.
+     *
+     * @param position The position of the item within the adapter's data set whose row id we want.
+     * @return The id of the item at the specified position.
+     */
+    @Override
+    public long getItemId(int position) {
+        // your particular dataset uses String IDs
+        // but you have to put something in this method
+
+        return position;
+    }
+
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link android.view.LayoutInflater#inflate(int, android.view.ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose view
+     *                    we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible to convert
+     *                    this view to display the correct data, this method can create a new view.
+     *                    Heterogeneous lists can specify their number of view types, so that this View is
+     *                    always of the right type (see {@link #getViewTypeCount()} and
+     *                    {@link #getItemViewType(int)}).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        // check if the view already exists
+        // if so, no need to inflate and findViewById again!
+        if (convertView == null) {
+
+            // Inflate the custom row layout from your XML.
+            convertView = mInflater.inflate(R.layout.row_book, null);
+
+            // create a new "Holder" with subviews
+            holder = new ViewHolder();
+            holder.thumbnailImageView = (ImageView) convertView.findViewById(R.id.img_thumbnail);
+            holder.titleTextView = (TextView) convertView.findViewById(R.id.text_title);
+            holder.authorTextView = (TextView) convertView.findViewById(R.id.text_author);
+
+            // hang onto this holder for future recyclage
+            convertView.setTag(holder);
+        } else {
+
+            // skip all the expensive inflation/findViewById
+            // and just get the holder you already made
+            holder = (ViewHolder) convertView.getTag();
+        }
+        // More code after this
+    // Get the current book's data in JSON form
+        JSONObject jsonObject = (JSONObject) getItem(position);
+
+    // See if there is a cover ID in the Object
+        if (jsonObject.has("cover_i")) {
+
+            // If so, grab the Cover ID out from the object
+            String imageID = jsonObject.optString("cover_i");
+
+            // Construct the image URL (specific to API)
+            String imageURL = IMAGE_URL_BASE + imageID + "-S.jpg";
+
+            // Use Picasso to load the image
+            // Temporarily have a placeholder in case it's slow to load
+            Picasso.with(mContext).load(imageURL).placeholder(R.mipmap.ic_books).into(holder.thumbnailImageView);
+        } else {
+
+            // If there is no cover ID in the object, use a placeholder
+            holder.thumbnailImageView.setImageResource(R.mipmap.ic_books);
+        }
+
+        // Grab the title and author from the JSON
+        String bookTitle = "";
+        String authorName = "";
+
+        if (jsonObject.has("title")) {
+            bookTitle = jsonObject.optString("title");
+        }
+
+        if (jsonObject.has("author_name")) {
+            authorName = jsonObject.optJSONArray("author_name").optString(0);
+        }
+
+        // Send these Strings to the TextViews for display
+        holder.titleTextView.setText(bookTitle);
+        holder.authorTextView.setText(authorName);
+
+
+        return convertView;
+
+}
+
+    // this is used so you only ever have to do
+    // inflation and finding by ID once ever per View
+    private static class ViewHolder {
+        public ImageView thumbnailImageView;
+        public TextView titleTextView;
+        public TextView authorTextView;
+    }
+
+    public void updateData(JSONArray jsonArray) {
+        // update the adapter's dataset
+        mJsonArray = jsonArray;
+        notifyDataSetChanged();
+    }
+
+}
--- app/src/main/java/com/example/dcarrion/omgandroid/MainActivity.java
+++ app/src/main/java/com/example/dcarrion/omgandroid/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.dcarrion.omgandroid;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 
@@ -22,7 +36,15 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
     EditText mainEditText;
     ListView mainListView;
     ArrayAdapter mArrayAdapter;
+    JSONAdapter mJSONAdapter;
     ArrayList mNameList = new ArrayList();
+    ShareActionProvider mShareActionProvider;
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+    private static final String QUERY_URL = "http://openlibrary.org/search.json?q=";
+
+    SharedPreferences mSharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +53,7 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
 
         //Access TextView in layout and sets it's text
         mainTextView = (TextView) findViewById(R.id.main_textview);
-        mainTextView.setText("Set in Java");
+        //mainTextView.setText("Set in Java");
 
         //Accesss the button defined in layout Xml and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
@@ -42,26 +64,62 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
 //
         //Access the listView
         mainListView = (ListView) findViewById(R.id.main_listview);
-//
+
         // Create an ArrayAdapter for the ListView
-        mArrayAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                mNameList);
-//
+        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,mNameList);
+
         //Set the listview to use the arrayAdapter
         mainListView.setAdapter(mArrayAdapter);
-//
-//        // Set this activity to react to the items being pressed
+
+        // Set this activity to react to the items being pressed
         mainListView.setOnItemClickListener(this);
+
+        displayWelcome();
+
+        // 10. Create a JSONAdapter for the ListView
+        //mJSONAdapter = new JSONAdapter(this, getLayoutInflater());
+
+        // Set the ListView to use the ArrayAdapter
+        //mainListView.setAdapter(mJSONAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider =  (ShareActionProvider) shareItem.getActionProvider();
+        }
+
+        // Create an Intent to share your content
+        setShareIntent();
+
         return true;
     }
 
+    private void setShareIntent() {
+
+        if (mShareActionProvider != null) {
+
+            // create an Intent with the contents of the TextView
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
     /**
      * Called when a view has been clicked.
      *
@@ -69,16 +127,20 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
      */
     @Override
     public void onClick(View v) {
-//        //Test the button
+        //Test the button
         //mainTextView.setText("Button Pressed");
 
-//        //Take what was typed in the Edit Text and use it in the TextView
+        //Take what was typed in the Edit Text and use it in the TextView
         mainTextView.setText(mainEditText.getText().toString() + " is learning Android Dev");
-//
-//        //Also add that value to the list shown in the listView
+
+        //Also add that value to the list shown in the listView
         mNameList.add(mainEditText.getText().toString());
         mArrayAdapter.notifyDataSetChanged();
 
+        // The text you want to share has changed and you need to update.
+        setShareIntent();
+        // 9. Take what was typed into the EditText and use in search
+        //queryBooks(mainEditText.getText().toString());
 
     }
 
@@ -101,5 +163,107 @@ public class MainActivity extends Activity implements View.OnClickListener,Adapt
 //        //Log the item position and contents to the console in Debug
         Log.d("omg Android", position + ":" + mNameList.get(position));
 
+    }
+
+    public void displayWelcome() {
+        //Access Device key-value storage
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+
+        //Read user's name or empty string if nothing is found
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+            //If name is valid, display a message welcoming the user
+            Toast.makeText(this,"Welcome back," + name + "!",Toast.LENGTH_LONG).show();
+        }
+    else {
+
+        // otherwise, show a dialog to ask for their name
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Hello!");
+        alert.setMessage("What is your name?");
+
+        // Create EditText for entry
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        // Make an "OK" button to save the name
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                // Grab the EditText's input
+                String inputName = input.getText().toString();
+
+                // Put it into memory (don't forget to commit!)
+                SharedPreferences.Editor e = mSharedPreferences.edit();
+                e.putString(PREF_NAME, inputName);
+                e.commit();
+
+                // Welcome the new user
+                Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Make a "Cancel" button
+        // that simply dismisses the alert
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {}
+        });
+
+        alert.show();
+    }
+    }
+
+    private void queryBooks(String searchString) {
+
+        // Prepare your search string to be put in a URL
+        // It might have reserved characters or something
+        String urlString = "";
+        try {
+            urlString = URLEncoder.encode(searchString, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+
+            // if this fails for some reason, let the user know why
+            e.printStackTrace();
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        // Create a client to perform networking
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        // Have the client get a JSONArray of data
+        // and define how to respond
+        client.get(QUERY_URL + urlString,
+                new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(JSONObject jsonObject) {
+
+                        // Display a "Toast" message
+                        // to announce your success
+                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+
+                        // 8. For now, just log results
+                        //Log.d("omg android", jsonObject.toString());
+
+                        // update the data in your custom method.
+                        mJSONAdapter.updateData(jsonObject.optJSONArray("docs"));
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+
+                        // Display a "Toast" message
+                        // to announce the failure
+                        Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
+
+                        // Log error message
+                        // to help solve any problems
+                        Log.e("omg android", statusCode + " " + throwable.getMessage());
+
+                    }
+                });
     }
 }
