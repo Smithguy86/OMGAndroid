aae931182f38505910d9a9d492f461e8089510b3
DANILO_CARRION <adida948@hotmail.com>
2015-06-18 14:39:22 -0400

    Created Activity to show the book image.

---
+++ app/src/main/java/com/example/dcarrion/omgandroid/DetailActivity.java
@@ -0,0 +1,79 @@
+package com.example.dcarrion.omgandroid;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by dcarrion on 6/18/2015.
+ */
+public class DetailActivity extends ActionBarActivity {
+
+    /*private static final String IMAGE_URL_BASE="http://covers.openlibrary.org/b/id/";
+    String mImageURL;
+    ShareActionProvider mShareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Tell the activity which XML layout is right
+        setContentView(R.layout.activity_detail);
+
+        // Enable the "Up" button for more navigation options
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Access the imageview from XML
+        ImageView imageView = (ImageView) findViewById(R.id.img_cover);
+
+        //Get the cover Id from the json response
+        String coverID= this.getIntent().getExtras().getString("coverID");
+
+        //Check for coverId validity
+        if (coverID.length() > 0) {
+
+            //Use the ID to create the imageURL
+            mImageURL = IMAGE_URL_BASE + coverID + "-L.jpg";
+
+            //Use Picaso(third party library..to load the image
+            Picasso.with(this).load(mImageURL).placeholder(R.mipmap.img_books_loading).into(imageView);
+        }
+    }*/
+
+/*    private void setShareIntent() {
+
+        // create an Intent with the contents of the TextView
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT,
+                "Book Recommendation!");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mImageURL);
+
+        // Make sure the provider knows
+        // it should work with that Intent
+        mShareActionProvider.setShareIntent(shareIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu
+        // this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider
+                    = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+        setShareIntent();
+
+        return true;
+    }*/
+
+
+
+}
